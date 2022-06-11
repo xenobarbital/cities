91ec6ca4ec97adbed5b322ca61d94234b1d1e4c0
David Gevorgyan <d.gevorgyan79@gmail.com>
2022-06-11 14:21:42 +0400

    add component structure

---
+++ src/Screens/CityList/CityList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './styles';
+
+const CityList = () => {
+  return (
+    <View>
+      <Text style={styles.text}>List</Text>
+    </View>
+  );
+};
+
+export default CityList;
---
+++ src/Screens/CityList/styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    color: '#fff',
+  },
+});
+
+export default styles;
