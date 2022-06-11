93a302e26e2c648f01d21ddf9976fda0f10f8f05
David Gevorgyan <d.gevorgyan79@gmail.com>
2022-06-11 15:12:15 +0400

    add vector icons lib

--- src/Screens/CityList/CityList.js
+++ src/Screens/CityList/CityList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
@@ -7,6 +8,7 @@ const CityList = () => {
   return (
     <View>
       <Text style={styles.text}>List</Text>
+      <Icon name="rocket" size={30} color="#900" />
     </View>
   );
 };
